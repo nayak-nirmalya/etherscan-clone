496e995f54fbb4b775b6dd14d6378e999d379c88
Nirmalya Nayak <n.nayak263@gmail.com>
2023-03-07 11:45:01 +0530

    added new search component

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import styles from "@/styles/Home.module.css";
 import Header from "@/components/Header";
+import Search from "@/components/Search";
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
       </Head>
       <section className={styles.main}>
         <Header />
+        <Search />
       </section>
     </>
   );
